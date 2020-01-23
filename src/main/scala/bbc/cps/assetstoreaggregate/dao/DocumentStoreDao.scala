4d4f578bb888409af9152ea8c6f5c06945e1ada2
Phill Barron <phillipbarron242@gmail.com>
2020-01-23 20:34:02 +0000

    added a find to the dao and service - not working at present

--- src/main/scala/bbc/cps/assetstoreaggregate/dao/DocumentStoreDao.scala
+++ src/main/scala/bbc/cps/assetstoreaggregate/dao/DocumentStoreDao.scala
@@ -3,11 +3,14 @@ package bbc.cps.assetstoreaggregate.dao
 import bbc.cps.assetstoreaggregate.Config.Mongo
 import bbc.cps.assetstoreaggregate.model.AssetDocument
 import org.mongodb.scala._
+import org.mongodb.scala.bson.conversions.Bson
 
 import scala.concurrent.Future
 
 trait DocumentStoreDao {
-  val mongoClient: MongoClient = MongoClient()
+  def find(filter: Bson): Future[Seq[Document]] = {
+      Mongo.assetCollection.find(filter).toFuture()
+  }
 
   def upsertAsset(assetDocument: AssetDocument): Future[Unit] = {
     Future.successful(Unit)
--- src/main/scala/bbc/cps/assetstoreaggregate/services/AssetStoreService.scala
+++ src/main/scala/bbc/cps/assetstoreaggregate/services/AssetStoreService.scala
@@ -4,7 +4,9 @@ import bbc.cps.assetstoreaggregate.dao.DocumentStoreDao
 import bbc.cps.assetstoreaggregate.model._
 import bbc.cps.assetstoreaggregate.monitoring.HistoryApiMonitor.monitor
 import bbc.cps.assetstoreaggregate.util.JsonFormats
+import org.json4s.JsonAST.JValue
 import org.mongodb.scala.bson.collection.immutable.Document
+import org.mongodb.scala.model.Filters._
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.Future
@@ -26,6 +28,10 @@ trait AssetStoreService extends JsonFormats {
     val document = optimoEventToAssetDocument(optimoEvent)
     println("WE ARE SAVING THE EVENT!!!!", document)
     documentStoreDao.create(Document(write(document)))
+  }
+
+  def getAsset(assetId: String): Future[JValue] = {
+    documentStoreDao.find(eq("assetId", assetId))
   }
 }
 
